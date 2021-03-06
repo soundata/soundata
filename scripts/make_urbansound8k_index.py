6d654e115a4b5a44ed109c4c27447460d779af1d
Justin Salamon <justin.salamon@gmail.com>
2021-03-05 17:11:05 -0800

    import md5 func from validate

--- scripts/make_urbansound8k_index.py
+++ scripts/make_urbansound8k_index.py
@@ -3,38 +3,24 @@ import hashlib
 import json
 import os
 import glob
+from soundata.validate import md5
 
 INDEX_PATH = "../soundata/datasets/indexes/urbansound8k_index.json"
 
 
-def md5(file_path):
-    """Get md5 hash of a file.
-
-    Parameters
-    ----------
-    file_path: str
-        File path.
-
-    Returns
-    -------
-    md5_hash: str
-        md5 hash of data in file_path
-    """
-    hash_md5 = hashlib.md5()
-    with open(file_path, "rb") as fhandle:
-        for chunk in iter(lambda: fhandle.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
-
 def make_index(data_path):
-    
+
     metadata_rel_path = os.path.join("metadata", "UrbanSound8K.csv")
 
     index = {
         "version": "1.0",
         "clips": {},
-        "metadata": {"UrbanSound8K.csv": [metadata_rel_path, md5(os.path.join(data_path, metadata_rel_path))]}
+        "metadata": {
+            "UrbanSound8K.csv": [
+                metadata_rel_path,
+                md5(os.path.join(data_path, metadata_rel_path)),
+            ]
+        },
     }
 
     for i in range(1, 11):
@@ -46,8 +32,12 @@ def make_index(data_path):
         for wf in wavfiles:
 
             clip_id = os.path.basename(wf).replace(".wav", "")
-            index["clips"][clip_id] = \
-                {"audio": [os.path.join("audio", "fold{}".format(i), os.path.basename(wf)), md5(wf)]}
+            index["clips"][clip_id] = {
+                "audio": [
+                    os.path.join("audio", "fold{}".format(i), os.path.basename(wf)),
+                    md5(wf),
+                ]
+            }
 
     with open(INDEX_PATH, "w") as fhandle:
         json.dump(index, fhandle, indent=2)
