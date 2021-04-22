56334a3abb1bc79bf89c6558d79dc765fcdfb5fd
genisplaja <genis.plaja01@estudiant.upf.edu>
2021-04-22 11:57:49 +0200

    subprocess.run fails, trying with call

--- soundata/datasets/fsd50k.py
+++ soundata/datasets/fsd50k.py
@@ -752,7 +752,7 @@ class Dataset(core.Dataset):
             "unsplit_" + zip_file[0].split(".")[1].split("_")[0] + ".zip",
         )
 
-        subprocess.run(["zip", "-s", "0", zip_path, "--out", output_path])
+        subprocess.call(["zip", "-s", "0", zip_path, "--out", output_path])
         download_utils.unzip(output_path, cleanup=True)
 
         # Remove zip files
