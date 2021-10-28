92cc72436e8577341301ee1bcd4b838cc0571569
iranroman <iran@ccrma.stanford.edu>
2021-10-28 11:14:43 -0400

    updates index

--- scripts/make_tau_nigens_sse_2021_index.py
+++ scripts/make_tau_nigens_sse_2021_index.py
@@ -10,60 +10,41 @@ DATASET_INDEX_PATH = "../soundata/datasets/indexes/tau_nigens_sse_2021_index.jso
 
 def _index_wav(index, data_path, formt, subset, split):
 
-    if subset == "development":
-        audio_path = os.path.join(
-                data_path,
-                formt+"_"+subset,
-                subset+"_"+split,
-        )
-    elif subset == "evaluation":
-        audio_path = os.path.join(
-                data_path,
-                formt+"_"+subset,
-        )
+    audio_path = os.path.join(
+            data_path,
+            formt+"_"+subset,
+            subset+"-"+split,
+    )
 
     wavfiles = glob.glob(os.path.join(audio_path, "*.wav"))
     wavfiles.sort()
 
     for wf in wavfiles:
 
-        if subset == "development":
-            clip_id = '{}/{}/{}'.format(
-                    formt+"_"+subset,
-                    subset+"_"+split,
-                    path.basename(wf).replace(".wav","")
-            )
+        clip_id = '{}/{}/{}'.format(
+                formt+"_"+subset,
+                subset+"-"+split,
+                os.path.basename(wf).replace(".wav","")
+        )
 
-            index["clips"][clip_id] = {
-                "audio": [
-                    os.path.join(formt+"_"+subset, subset+"_"+split, os.path.basename(wf)),
-                    md5(wf),
-                ],
-            }
-        elif subset == "evaluation":
-            clip_id = '{}/{}'.format(
-                    formt+"_"+subset,
-                    path.basename(wf).replace(".wav","")
-            )
-
-            index["clips"][clip_id] = {
-                "audio": [
-                    os.path.join(formt+"_"+subset, os.path.basename(wf)),
-                    md5(wf),
-                ],
-            }
+        index["clips"][clip_id] = {
+            "audio": [
+                os.path.join(formt+"_"+subset, subset+"-"+split, os.path.basename(wf)),
+                md5(wf),
+            ],
+        }
 
     return index
 
 def _index_event(index, data_path, formt, annotation_subset, subset, split):
 
-    if subset == "development":
+    if subset == "dev":
         annotation_path = os.path.join(
                 data_path,
                 annotation_subset,
-                subset+"_"+split,
+                subset+"-"+split,
         )
-    elif subset == "evaluation":
+    elif subset == "eval":
         annotation_path = os.path.join(
                 data_path,
                 annotation_subset,
@@ -74,22 +55,18 @@ def _index_event(index, data_path, formt, annotation_subset, subset, split):
 
     for af in annotationfiles:
 
-        if subset == "development":
-            clip_id = '{}/{}/{}'.format(
-                    formt+"_"+subset,
-                    subset+"_"+split,
-                    os.path.basename(af).replace(".csv", "")
-            )
+        clip_id = '{}/{}/{}'.format(
+                formt+"_"+subset,
+                subset+"-"+split,
+                os.path.basename(af).replace(".csv", "")
+        )
+        if subset == "dev":
 
             index["clips"][clip_id]["events"] = [
-                    os.path.join(annotation_subset, subset+"_"+split, os.path.basename(af)),
+                    os.path.join(annotation_subset, subset+"-"+split, os.path.basename(af)),
                     md5(af),
                 ]
-        elif subset == "evaluation":
-            clip_id = '{}/{}'.format(
-                    formt+"_"+subset,
-                    os.path.basename(af).replace(".csv", "")
-            )
+        elif subset == "eval":
 
             index["clips"][clip_id]["events"] = [
                     os.path.join(annotation_subset, os.path.basename(af)),
@@ -132,18 +109,18 @@ def make_index(data_path):
 
         for formt in formats:
 
-            for s in split:
+            if subset == "development":
 
-                if subset == "development":
+                for s in split:
 
                     index = _index_wav(index, data_path, formt, "dev", s)
 
                     index = _index_event(index, data_path, formt, annotations[subset], "dev", s)
 
-                elif subset == "evaluation":
+            elif subset == "evaluation":
 
-                    index = _index_wav(index, data_path, formt, "eval", s)
-                    index = _index_event(index, data_path, formt, annotations[subset], "eval", s)
+                index = _index_wav(index, data_path, formt, "eval", "test")
+                index = _index_event(index, data_path, formt, annotations[subset], "eval", "test")
 
 
     with open(DATASET_INDEX_PATH, "w") as fhandle:
