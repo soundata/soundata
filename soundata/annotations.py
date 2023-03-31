0bbe079ecfa1a4b83ed2045a43530d2609d323e4
Harsh Palan <harshpalan14@gmail.com>
2023-03-31 16:34:45 -0400

    Updating Event Class for Spatial Events

--- soundata/annotations.py
+++ soundata/annotations.py
@@ -8,6 +8,19 @@ TIME_UNITS = {"seconds": "seconds", "milliseconds": "milliseconds"}
 #: Label units
 LABEL_UNITS = {"open": "no strict schema or units"}
 
+#: Azimuth units
+AZIMUTH_UNITS = {
+    "radians": "values in the interval [-2*pi, 2*pi]",
+    "degrees": "values in the interval [-360, 360]",
+}
+
+#: Distance units
+DISTANCE_UNITS = {
+    "meters": "meters",
+    "centimeters": "centimeters",
+    "milimeters": "milimeters",
+}
+
 
 class Annotation(object):
     """Annotation base class"""
@@ -50,12 +63,32 @@ class Events(Annotation):
         confidence (np.ndarray or None): array of confidence values, float in [0, 1]
         labels_unit (str): labels unit, one of LABELS_UNITS
         intervals_unit (str): intervals unit, one of TIME_UNITS
-
+        azimuth (np.ndarray or None):
+        azimuth_unit (str): azimuth unit, one of AZIMUTH_UNITS
+        elevation (np.ndarray or None):
+        elevation_unit (str): elevation unit, one of AZIMUTH_UNITS
+        distance (np.ndarray or None):
+        distance_unit (str): distance unit, one of DISTANCE_UNITS
+        cartesian_coord (np.ndarray or None):
+        cartesian_coord_unit (str): cartesian_coord unit, one of DISTANCE_UNITS
 
     """
 
     def __init__(
-        self, intervals, intervals_unit, labels, labels_unit, confidence=None
+        self,
+        intervals,
+        intervals_unit,
+        labels,
+        labels_unit,
+        confidence=None,
+        azimuth=None,
+        azimuth_unit=None,
+        elevation=None,
+        elevation_unit=None,
+        distance=None,
+        distance_unit=None,
+        cartesian_coord=None,
+        cartesian_coord_unit=None,
     ) -> None:
         validate_array_like(intervals, np.ndarray, float)
         validate_array_like(labels, list, str)
@@ -65,12 +98,33 @@ class Events(Annotation):
         validate_confidence(confidence)
         validate_unit(labels_unit, LABEL_UNITS)
         validate_unit(intervals_unit, TIME_UNITS)
+        validate_array_like(azimuth, np.ndarray, float, none_allowed=True)
+        validate_array_like(elevation, np.ndarray, float, none_allowed=True)
+        validate_array_like(distance, np.ndarray, float, none_allowed=True)
+        validate_array_like(cartesian_coord, np.ndarray, float, none_allowed=True)
+        validate_lengths_equal([azimuth, elevation, distance, cartesian_coord])
+        validate_azimuth(azimuth, azimuth_unit, allow_none=True)
+        validate_distance(distance, allow_none=True)
+        validate_azimuth(elevation, elevation_unit, allow_none=True)
+        validate_cartesian_coord(cartesian_coord, allow_none=True)
+        validate_unit(azimuth_unit, AZIMUTH_UNITS, allow_none=True)
+        validate_unit(distance_unit, DISTANCE_UNITS, allow_none=True)
+        validate_unit(elevation_unit, AZIMUTH_UNITS, allow_none=True)
+        validate_unit(cartesian_coord_unit, DISTANCE_UNITS, allow_none=True)
 
         self.intervals = intervals
         self.intervals_unit = intervals_unit
         self.labels = labels
         self.labels_unit = labels_unit
         self.confidence = confidence
+        self.azimuth = azimuth
+        self.azimuth_unit = azimuth_unit
+        self.elevation = elevation
+        self.elevation_unit = elevation_unit
+        self.distance = distance
+        self.distance_unit = distance_unit
+        self.cartesian_coord = cartesian_coord
+        self.cartesian_coord_unit = cartesian_coord_unit
 
 
 class MultiAnnotator(Annotation):
@@ -272,3 +326,46 @@ def validate_unit(unit, unit_values, allow_none=False):
 
     if unit not in unit_values:
         raise ValueError("unit={} is not one of {}".format(unit, unit_values))
+
+
+def validate_azimuth(azimuth, azimuth_unit=None, allow_none=False):
+
+    if allow_none and not azimuth_unit:
+        return
+
+    if azimuth_unit == "radians":
+        if any([a < -2 * np.pi for a in azimuth]) or any(
+            [a > 2 * np.pi for a in azimuth]
+        ):
+            raise ValueError(
+                "azimuth with unit 'radians' should be between -2*np.pi and 2*np.pi. "
+                + "Found values outside that interval."
+            )
+
+    if azimuth_unit == "degrees":
+        if any([a < -360 for a in azimuth]) or any([a > 360 for a in azimuth]):
+            raise ValueError(
+                "azimuth with unit 'degrees' should be between -360 and 360. "
+                + "Found values outside that interval."
+            )
+
+
+def validate_distance(distance, allow_none=False):
+    if allow_none:
+        return
+
+    if any([d < 0 for d in distance]):
+        raise ValueError(
+            "distance should be bigger or equal than zero. " + "Found negative values."
+        )
+
+
+def validate_cartesian_coord(cartesian_coord, allow_none=False):
+    if allow_none:
+        return
+
+    if not np.shape(cartesian_coord)[1] == 3:
+        raise ValueError(
+            f"cartesian coordinates should have three columns corresponding to x, y and z coordinates. "
+            f"Found {len(cartesian_coord)} columns instead."
+        )
--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -2,7 +2,6 @@ import sys
 import pytest
 import numpy as np
 
-import soundata
 from soundata import annotations
 
 
@@ -21,7 +20,7 @@ def test_repr():
     )
     assert (
         event_data.__repr__()
-        == "Events(confidence, intervals, intervals_unit, labels, labels_unit)"
+        == "Events(azimuth, azimuth_unit, cartesian_coord, cartesian_coord_unit, confidence, distance, distance_unit, elevation, elevation_unit, intervals, intervals_unit, labels, labels_unit)"
     )
 
 
@@ -54,6 +53,110 @@ def test_events():
     assert np.allclose(events.intervals, intervals)
     assert events.labels == labels
     assert np.allclose(events.confidence, confidence)
+
+    # test bad data
+    bad_intervals = np.array([[1.0, 0.0], [1.5, 3.0], [2.0, 3.0]])
+    pytest.raises(
+        ValueError,
+        annotations.Events,
+        bad_intervals,
+        "seconds",
+        labels,
+        "open",
+        confidence,
+    )
+
+    bad_labels = ["Siren", "Laughter", 5]
+    pytest.raises(
+        TypeError,
+        annotations.Events,
+        intervals,
+        "seconds",
+        bad_labels,
+        "open",
+        confidence,
+    )
+
+    bad_confidence = np.array([1, 0.5, -0.2])
+    pytest.raises(
+        ValueError,
+        annotations.Events,
+        intervals,
+        "seconds",
+        labels,
+        "open",
+        bad_confidence,
+    )
+
+    # test units
+
+    with pytest.raises(ValueError):
+        annotations.Events(intervals, "seconds", labels, "bad_unit", confidence)
+
+    with pytest.raises(ValueError):
+        annotations.Events(intervals, "bad_unit", labels, "open", confidence)
+
+    with pytest.raises(TypeError):
+        annotations.Events(intervals, labels, confidence)
+
+    with pytest.raises(TypeError):
+        annotations.Events(intervals, labels, "open", confidence)
+
+    with pytest.raises(TypeError):
+        annotations.Events(intervals, "seconds", labels, confidence)
+
+
+def test_spatial_events():
+    # test good data
+    intervals = np.array([[1.0, 2.0], [1.5, 3.0]])
+    labels = ["Siren", "Car"]
+    confidence = np.array([1.0, 0.0])
+    azimuth_degrees = np.array([0, 90]).astype(float)
+    azimuth_radians = np.array([0, np.pi / 2])
+    distance = np.array([1, 1]).astype(float)
+    elevation_degrees = np.array([0, 90]).astype(float)
+    elevation_radians = np.array([0, np.pi / 2])
+    cartesian_coord = np.array([[1, 0, 0], [1, 1, 1]]).astype(float)
+    sparial_events_deg = annotations.Events(
+        intervals,
+        "seconds",
+        labels,
+        "open",
+        confidence,
+        azimuth_degrees,
+        "degrees",
+        elevation_degrees,
+        "degrees",
+        distance,
+        "meters",
+        cartesian_coord,
+        "meters",
+    )
+    sparial_events_rad = annotations.Events(
+        intervals,
+        "seconds",
+        labels,
+        "open",
+        confidence,
+        azimuth_radians,
+        "radians",
+        elevation_radians,
+        "radians",
+        distance,
+        "meters",
+        cartesian_coord,
+        "meters",
+    )
+
+    assert np.allclose(sparial_events_deg.intervals, intervals)
+    assert sparial_events_deg.labels == labels
+    assert np.allclose(sparial_events_deg.confidence, confidence)
+    assert np.allclose(sparial_events_deg.azimuth, azimuth_degrees)
+    assert np.allclose(sparial_events_deg.elevation, elevation_degrees)
+    assert np.allclose(sparial_events_deg.cartesian_coord, cartesian_coord)
+
+    assert np.allclose(sparial_events_rad.azimuth, azimuth_radians)
+    assert np.allclose(sparial_events_rad.elevation, elevation_radians)
 
     # test bad data
     bad_intervals = np.array([[1.0, 0.0], [1.5, 3.0], [2.0, 3.0]])
