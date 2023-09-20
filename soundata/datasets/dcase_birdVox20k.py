dafcaced4a82b51db9671987d88acb79fa9a0cc8
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2023-09-20 15:08:42 -0400

    edited the description

--- soundata/datasets/dcase_birdVox20k.py
+++ soundata/datasets/dcase_birdVox20k.py
@@ -6,123 +6,62 @@
     Created By
     ----------
 
-    Justin Salamon*^, Christopher Jacoby* and Juan Pablo Bello*
-    * Music and Audio Research Lab (MARL), New York University, USA
-    ^ Center for Urban Science and Progress (CUSP), New York University, USA
-    https://urbansounddataset.weebly.com/
-    https://steinhardt.nyu.edu/marl
-    http://cusp.nyu.edu/
+    Vincent Lostanlen*^#, Justin Salamon^#, Andrew Farnsworth*, Steve Kelling*, and Juan Pablo Bello^#
+    * Cornell Lab of Ornithology (CLO)
+    ^ Center for Urban Science and Progress, New York University
+    # Music and Audio Research Lab, New York University
 
     Version 1.0
-
 
     Description
     -----------
 
-    This dataset contains 8732 labeled sound excerpts (<=4s) of urban sounds from 10 classes: air_conditioner, car_horn, 
-    children_playing, dog_bark, drilling, engine_idling, gun_shot, jackhammer, siren, and street_music. The classes are 
-    drawn from the urban sound taxonomy described in the following article, which also includes a detailed description of 
-    the dataset and how it was compiled:
-
-    .. code-block:: latex
-        J. Salamon, C. Jacoby and J. P. Bello, "A Dataset and Taxonomy for Urban Sound Research", 
-        22nd ACM International Conference on Multimedia, Orlando USA, Nov. 2014.
-
-    All excerpts are taken from field recordings uploaded to www.freesound.org. The files are pre-sorted into ten folds
-    (folders named fold1-fold10) to help in the reproduction of and comparison with the automatic classification results
-    reported in the article above.
-
-    In addition to the sound excerpts, a CSV file containing metadata about each excerpt is also provided.
-
+    The BirdVox-DCASE-20k dataset contains 20,000 ten-second audio recordings. These recordings come from ROBIN autonomous recording units, placed near Ithaca, NY, USA during the fall 2015. They were captured on the night of September 23rd, 2015, by six different sensors, originally numbered 1, 2, 3, 5, 7, and 10.
+    Out of these 20,000 recording, 10,017 (50.09%) contain at least one bird vocalization (either song, call, or chatter).
+    The dataset is a derivative work of the BirdVox-full-night dataset [1], containing almost as much data but formatted into ten-second excerpts rather than ten-hour full night recordings.
+    In addition, the BirdVox-DCASE-20k dataset is provided as a development set in the context of the "Bird Audio Detection" challenge, organized by DCASE (Detection and Classification of Acoustic Scenes and Events) and the IEEE Signal Processing Society.
+    The dataset can be used, among other things, for the development and evaluation of bioacoustic classification models.
 
     Audio Files Included
     --------------------
 
-    8732 audio files of urban sounds (see description above) in WAV format. The sampling rate, bit depth, and number of 
-    channels are the same as those of the original file uploaded to Freesound (and hence may vary from file to file).
-
+    20,000 ten-second audio recordings (see description above) in WAV format. The wav folder contains the recordings as WAV files, sampled at 44,1 kHz, with a single channel (mono). The original sample rate was 24 kHz.
 
     Meta-data Files Included
     ------------------------
 
-    UrbanSound8k.csv
-
-    This file contains meta-data information about every audio file in the dataset. This includes:
-
-    * slice_file_name: 
-    The name of the audio file. The name takes the following format: [fsID]-[classID]-[occurrenceID]-[sliceID].wav, where:
-    [fsID] = the Freesound ID of the recording from which this excerpt (slice) is taken
-    [classID] = a numeric identifier of the sound class (see description of classID below for further details)
-    [occurrenceID] = a numeric identifier to distinguish different occurrences of the sound within the original recording
-    [sliceID] = a numeric identifier to distinguish different slices taken from the same occurrence
-
-    * fsID:
-    The Freesound ID of the recording from which this excerpt (slice) is taken
-
-    * start
-    The start time of the slice in the original Freesound recording
-
-    * end:
-    The end time of slice in the original Freesound recording
-
-    * salience:
-    A (subjective) salience rating of the sound. 1 = foreground, 2 = background.
-
-    * fold:
-    The fold number (1-10) to which this file has been allocated.
-
-    * classID:
-    A numeric identifier of the sound class:
-    0 = air_conditioner
-    1 = car_horn
-    2 = children_playing
-    3 = dog_bark
-    4 = drilling
-    5 = engine_idling
-    6 = gun_shot
-    7 = jackhammer
-    8 = siren
-    9 = street_music
-
-    * class:
-    The class name: air_conditioner, car_horn, children_playing, dog_bark, drilling, engine_idling, gun_shot, jackhammer, 
-    siren, street_music.
-
+    A table containing a binary label "hasbird" associated to every recording in BirdVox-DCASE-20k is available on the website of the DCASE "Bird Audio Detection" challenge: http://machine-listening.eecs.qmul.ac.uk/bird-audio-detection-challenge/
+    These labels were automatically derived from the annotations of avian flight call events in the BirdVox-full-night dataset.
 
     Please Acknowledge UrbanSound8K in Academic Research
     ----------------------------------------------------
 
-    When UrbanSound8K is used for academic research, we would highly appreciate it if scientific publications of works 
-    partly based on the UrbanSound8K dataset cite the following publication:
+    When BirdVox-70k is used for academic research, we would highly appreciate it if  scientific publications of works partly based on this dataset cite the  following publication:
 
     .. code-block:: latex
-        J. Salamon, C. Jacoby and J. P. Bello, "A Dataset and Taxonomy for Urban Sound Research", 
-        22nd ACM International Conference on Multimedia, Orlando USA, Nov. 2014.
-
-    The creation of this dataset was supported by a seed grant by NYU's Center for Urban Science and Progress (CUSP).
-
+        V. Lostanlen, J. Salamon, A. Farnsworth, S. Kelling, J. Bello. "BirdVox-full-night: a dataset and benchmark for avian flight call detection", Proc. IEEE ICASSP, 2018.
+    
+    The creation of this dataset was supported by NSF grants 1125098 (BIRDCAST) and 1633259 (BIRDVOX), a Google Faculty Award, the Leon Levy Foundation, and two anonymous donors.
 
     Conditions of Use
     -----------------
 
-    Dataset compiled by Justin Salamon, Christopher Jacoby and Juan Pablo Bello. All files are excerpts of recordings
-    uploaded to www.freesound.org. Please see FREESOUNDCREDITS.txt for an attribution list.
-    
-    The UrbanSound8K dataset is offered free of charge for non-commercial use only under the terms of the Creative Commons
-    Attribution Noncommercial License (by-nc), version 3.0: http://creativecommons.org/licenses/by-nc/3.0/
-    
-    The dataset and its contents are made available on an "as is" basis and without warranties of any kind, including 
-    without limitation satisfactory quality and conformity, merchantability, fitness for a particular purpose, accuracy or 
-    completeness, or absence of errors. Subject to any liability that may not be excluded or limited by law, NYU is not 
-    liable for, and expressly excludes, all liability for loss or damage however and whenever caused to anyone by any use of
-    the UrbanSound8K dataset or any part of it.
+    Dataset created by Vincent Lostanlen, Justin Salamon, Andrew Farnsworth, Steve Kelling, and Juan Pablo Bello.
 
+    The BirdVox-DCASE-20k dataset is offered free of charge under the terms of the Creative  Commons Attribution 4.0 International (CC BY 4.0) license:
+    https://creativecommons.org/licenses/by/4.0/
+
+    The dataset and its contents are made available on an "as is" basis and without  warranties of any kind, including without limitation satisfactory quality and  conformity, merchantability, fitness for a particular purpose, accuracy or  completeness, or absence of errors. Subject to any liability that may not be excluded or limited by law, Cornell Lab of Ornithology is not liable for, and expressly excludes all liability for, loss or damage however and whenever caused to anyone by any use of the BirdVox-DCASE-20k dataset or any part of it.
 
     Feedback
     --------
 
-    Please help us improve UrbanSound8K by sending your feedback to: justin.salamon@nyu.edu
-    In case of a problem report please include as many details as possible.
+    Please help us improve BirdVox-DCASE-20k by sending your feedback to:
+    * Vincent Lostanlen: vincent.lostanlen@gmail.com for feedback regarding data pre-processing,
+    * Andrew Farnsworth: af27@cornell.edu for feedback regarding data collection and ornithology, or
+    * Dan Stowell: dan.stowell@qmul.ac.uk for feedback regarding the DCASE "Bird Audio Detection" challenge.
+
+    In case of a problem, please include as many details as possible.
 
 """
 
