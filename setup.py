e1ae599eed00a8bc25700b1a00550c80630c1d49
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2024-05-23 15:18:49 +0530

    addition to remove test files from pip

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ version_module = version_sfl.load_module()
 if __name__ == "__main__":
     setup(
         version=version_module.version,
-        packages=find_packages(exclude=["test", "*.test", "*.test.*"]),
+                packages=find_packages(exclude=["test", "*.test", "*.test.*","tests", "tests.*", "*.tests.*", "tests/__pycache__", "tests/__pycache__/*"]),
+
     )
